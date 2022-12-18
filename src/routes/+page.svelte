b68bad4da8f2c007dd7588f40cea845f67ae89ce
Vladimir Banderov <like.a.flower.in.a.basement@gmail.com>
2022-12-18 16:23:34 -0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Clock from "./clock.svelte";
+
+	let name = 'Vladimir';
+
+</script>
+
+<div class="w-screen h-screen bg-black">
+	<h1 class=" text-white">Welcome to SvelteKit, {name}</h1>
+
+	<Clock />
+</div>
---
+++ src/routes/clock.svelte
@@ -0,0 +1,16 @@
+
+
+<script>
+	import { time } from './stores';
+
+	const formatter = new Intl.DateTimeFormat('en', {
+		hour12: true,
+		hour: 'numeric',
+		minute: '2-digit',
+		second: '2-digit'
+	});
+
+	$: timeFormatted = formatter.format($time);
+</script>
+
+<h1 class=" text-white">Current time {timeFormatted}</h1>
